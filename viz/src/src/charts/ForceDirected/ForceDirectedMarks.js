1e141fb7c026fa9408b0c57c24c3b8ed85542589
Zachary Young <zacharyyoung95@gmail.com>
2020-12-01 11:52:55 -0700

    Add ForceDirected classes

--- viz/src/src/charts/ForceDirected/ForceDirectedMarks.js
+++ viz/src/src/charts/ForceDirected/ForceDirectedMarks.js
@@ -1,3 +1,126 @@
-export default class ForceDirectedMarks {
+// Keep the import below.
+// import { _ } from '@analytics/core';
+import { _ } from './../../analytics/core';
 
+export default class ForceDirectedMarks {
+  // TODO: Update this so it is no longer hard coded later on. 
+  getMarks(opts) {
+		const marks = _.compact([{
+      "name": "nodes",
+      "type": "symbol",
+      "zindex": 1,
+
+      "from": {
+        "data": "node-data"
+      },
+      "on": [{
+          "trigger": "fix",
+          "modify": "node",
+          "values": "fix === true ? {fx: node.x, fy: node.y} : {fx: fix[0], fy: fix[1]}"
+        },
+        {
+          "trigger": "!fix",
+          "modify": "node",
+          "values": "{fx: null, fy: null}"
+        }
+      ],
+
+      "encode": {
+        "enter": {
+          "fill": {
+            "scale": "color",
+            "field": "group"
+          },
+          "stroke": {
+            "value": "white"
+          }
+        },
+        "update": {
+          "size": {
+            "signal": "2 * nodeRadius * nodeRadius"
+          },
+          "cursor": {
+            "value": "pointer"
+          }
+        }
+      },
+
+      "transform": [{
+        "type": "force",
+        "iterations": 300,
+        "restart": {
+          "signal": "restart"
+        },
+        "static": {
+          "signal": "static"
+        },
+        "signal": "force",
+        "forces": [{
+            "force": "center",
+            "x": {
+              "signal": "cx"
+            },
+            "y": {
+              "signal": "cy"
+            }
+          },
+          {
+            "force": "collide",
+            "radius": {
+              "signal": "nodeRadius"
+            }
+          },
+          {
+            "force": "nbody",
+            "strength": {
+              "signal": "nodeCharge"
+            }
+          },
+          {
+            "force": "link",
+            "links": "link-data",
+            "distance": {
+              "signal": "linkDistance"
+            }
+          }
+        ]
+      }]
+    },
+    {
+      "type": "path",
+      "from": {
+        "data": "link-data"
+      },
+      "interactive": false,
+      "encode": {
+        "update": {
+          "stroke": {
+            "value": "#ccc"
+          },
+          "strokeWidth": {
+            "value": 0.5
+          }
+        }
+      },
+      "transform": [{
+        "type": "linkpath",
+        "require": {
+          "signal": "force"
+        },
+        "shape": "line",
+        "sourceX": "datum.source.x",
+        "sourceY": "datum.source.y",
+        "targetX": "datum.target.x",
+        "targetY": "datum.target.y"
+      }]
+    }
+  ]);
+
+		return marks;
+	}
+
+  static create(opts) {
+		const instance = new this();
+		return instance.getMarks(opts);
+	}
 }
--- viz/src/src/charts/ForceDirected/ForceDirectedSignals.js
+++ viz/src/src/charts/ForceDirected/ForceDirectedSignals.js
@@ -1,3 +1,100 @@
-export default class ForceDirectedSignals {
+// Keep the import below.
+// import { _ } from '@analytics/core';
+import { _ } from './../../analytics/core';
 
+export default class ForceDirectedSignals {
+  // TODO: Update this so it is no longer hard coded later on. 
+  getSignals(opts) {
+    return _.compact([
+      {
+        "name": "cx",
+        "update": "width / 2"
+      },
+      {
+        "name": "cy",
+        "update": "height / 2"
+      },
+      {
+        "name": "nodeRadius",
+        "value": 8,
+        "bind": {
+          "input": "range",
+          "min": 1,
+          "max": 50,
+          "step": 1
+        }
+      },
+      {
+        "name": "nodeCharge",
+        "value": -30,
+        "bind": {
+          "input": "range",
+          "min": -100,
+          "max": 10,
+          "step": 1
+        }
+      },
+      {
+        "name": "linkDistance",
+        "value": 30,
+        "bind": {
+          "input": "range",
+          "min": 5,
+          "max": 100,
+          "step": 1
+        }
+      },
+      {
+        "name": "static",
+        "value": true,
+        "bind": {
+          "input": "checkbox"
+        }
+      },
+      {
+        "description": "State variable for active node fix status.",
+        "name": "fix",
+        "value": false,
+        "on": [{
+            "events": "symbol:mouseout[!event.buttons], window:mouseup",
+            "update": "false"
+          },
+          {
+            "events": "symbol:mouseover",
+            "update": "fix || true"
+          },
+          {
+            "events": "[symbol:mousedown, window:mouseup] > window:mousemove!",
+            "update": "xy()",
+            "force": true
+          }
+        ]
+      },
+      {
+        "description": "Graph node most recently interacted with.",
+        "name": "node",
+        "value": null,
+        "on": [{
+          "events": "symbol:mouseover",
+          "update": "fix === true ? item() : node"
+        }]
+      },
+      {
+        "description": "Flag to restart Force simulation upon data changes.",
+        "name": "restart",
+        "value": false,
+        "on": [{
+          "events": {
+            "signal": "fix"
+          },
+          "update": "fix && fix.length"
+        }]
+      }
+    ]); 
+  }
+
+  static create(opts) {
+		const instance = new this();
+		return instance.getSignals(opts);
+	}
 }
