e9a83ed983ed8b9719e5700b6975af8e4acbb2c8
root <root@DESKTOP-ALP2BMH.localdomain>
2020-12-01 09:24:51 -0700

    Added main and parser class

--- api/app.js
+++ api/app.js
@@ -1,30 +1,6 @@
 const fetch = require("node-fetch");
 
-var temp_token = "<token>";
-var temp_dateRange = "2020-09-30T00:00:00.000/2020-11-04T00:00:00.000";
-var temp_dimension = "variables/browser";
-var temp_company = "<companyId>";
-var temp_user = "<userId>";
-
-var simple = false;
-
-if (simple) {
-    getRequest(temp_token, temp_company, temp_user, temp_dateRange, temp_dimension)
-        .then(res => {
-            console.log(res);
-            // res.rows.forEach(element => {
-            //     console.log(element)
-            // }); 
-        });
-} else {
-    getSequenceRequest(temp_token, temp_company, temp_user, temp_dateRange, temp_dimension)
-        .then(res => {
-            // console.log(res);
-            res.rows.forEach(element => {
-                console.log(element)
-            });
-        });
-}
+exports.getRequest = getRequest
 
 async function getRequest(token, company, user, dateRange, dimension) {
     const res = await fetch("https://appservice5.omniture.com/analytics/1.0/reports?allowRemoteLoad=default&useCache=true&includeOberonXml=false&includePlatformPredictiveObjects=false", {
@@ -48,12 +24,12 @@ async function getRequest(token, company, user, dateRange, dimension) {
         },
         "referrer": "https://sc5.omniture.com/",
         "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": `{
+        "body": JSON.stringify({
             "rsid": "a4t-test.geometrixxqc",
             "globalFilters": [
                 {
                     "type": "dateRange",
-                    "dateRange": "${dateRange}"
+                    "dateRange": dateRange
                 }
             ],
             "metricContainer": {
@@ -65,10 +41,10 @@ async function getRequest(token, company, user, dateRange, dimension) {
                     }
                 ]
             },
-            "dimension": "${dimension}",
+            "dimension": dimension,
             "settings": {
                 "countRepeatInstances": true,
-                "limit": 50,
+                "limit": 9,
                 "page": 0,
                 "nonesBehavior": "return-nones"
             },
@@ -78,14 +54,14 @@ async function getRequest(token, company, user, dateRange, dimension) {
                     "col-min"
                 ]
             }
-        }`,
+        }),
         "method": "POST",
         "mode": "cors"
     });
     const json = await res.json();
-
     return json;
 }
+
 
 async function getSequenceRequest(token, company, user, dateRange, dimension) {
     const res = await fetch("https://appservice5.omniture.com/analytics/1.0/reports?allowRemoteLoad=default&useCache=true&includeOberonXml=false&includePlatformPredictiveObjects=false", {
@@ -169,5 +145,140 @@ async function getSequenceRequest(token, company, user, dateRange, dimension) {
 
     return json;
 }
+
+
+
+// trying to test
+async function getSeqRequest(token, company, user, dateRange, dimension) {
+    const res = await fetch("https://appservice5.omniture.com/analytics/1.0/reports?allowRemoteLoad=default&useCache=true&includeOberonXml=false&includePlatformPredictiveObjects=false", 
+    {
+        "headers": {
+                    "accept": "application/json",
+                    "accept-language": "en-US,en;q=0.9,zh-TW;q=0.8,zh-CN;q=0.7,zh;q=0.6",
+                    "authorization": token,
+                    "cache-control": "no-cache",
+                    "content-type": "application/json;charset=UTF-8",
+                    "pragma": "no-cache",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-site",
+                    "x-proxy-company-id": company,
+                    "x-proxy-userid": user,
+                    "x-request-client-type": "AW",
+                    "x-request-entity-id": "5ef391a23126e227c822f579",
+                    "x-request-entity-name-base64": "U2hhcmVkIHdpdGggYWxsIHRvIG5vbi1hZG1pbg==",
+                    "x-request-id": "15ba90be825944d681cfd268e2517a04",
+                    "x-request-timing-group-id": "cb073b95f7b34ff8ad8067d547494234"
+                },
+                "referrer": "https://sc5.omniture.com/",
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": JSON.stringify({
+                    "rsid": "a4t-test.geometrixxqc",
+                    "globalFilters": [
+                        {
+                            "type": "dateRange",
+                            "dateRange": "2020-11-04T00:00:00.000/2020-12-02T00:00:00.000"
+                        }
+                    ],
+                    "metricContainer": {
+                        "metrics": [
+                            {
+                                "columnId": "1",
+                                "id": "metrics/occurrences",
+                                "filters": [
+                                    "0"
+                                ]
+                            }
+                        ],
+                        "metricFilters": [
+                            {
+                                "id": "0",
+                                "type": "segment",
+                                "segmentDefinition": {
+                                    "container": {
+                                        "func": "container",
+                                        "context": "hits",
+                                        "pred": {
+                                            "func": "sequence-prefix",
+                                            "context": "visitors",
+                                            "stream": [
+                                                {
+                                                    "func": "container",
+                                                    "context": "hits",
+                                                    "pred": {
+                                                        "str": "Home",
+                                                        "val": {
+                                                            "func": "attr",
+                                                            "name": "variables/page"
+                                                        },
+                                                        "func": "streq",
+                                                        "description": "Page"
+                                                    },
+                                                },
+                                                {
+                                                    "func": "dimension-restriction",
+                                                    "count": 1,
+                                                    "limit": "within",
+                                                    "attribute": {
+                                                        "func": "attr",
+                                                        "name": "variables/page",
+                                                        "description": "Page"
+                                                    }
+                                                },
+                                                {
+                                                    "func": "container",
+                                                    "context": "hits",
+                                                    "pred": {
+                                                        "str": "Shopping Cart|Cart Details",
+                                                        "val": {
+                                                            "func": "attr",
+                                                            "name": "variables/page"
+                                                        },
+                                                        "func": "streq",
+                                                        "description": "Page"
+                                                    },
+                                                },
+                                                {
+                                                    "func": "exclude-next-checkpoint"
+                                                },
+                                                {
+                                                    "func": "container",
+                                                    "context": "hits",
+                                                    "pred": {
+                                                        "val": {
+                                                            "func": "attr",
+                                                            "name": "variables/page"
+                                                        },
+                                                        "func": "exists",
+                                                        "description": "Page"
+                                                    },
+                                                }
+                                            ],
+                                        },
+                                    },
+                                    "func": "segment",
+                                    "version": [
+                                        1,
+                                        0,
+                                        0
+                                    ]
+                                }
+                            }
+                        ]
+                    },
+                    "dimension": "variables/page",
+                    "settings": {
+                        "countRepeatInstances": true,
+                        "limit": 5
+                    }
+                }),
+                "method": "POST",
+                "mode": "cors"
+            });
+    const json = await res.json();
+
+    return json;
+}
+
 
 
