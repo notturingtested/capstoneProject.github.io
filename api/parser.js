4e0b3c7e2e22629ef36606809f4a64c045629ae7
haotsai <tsaizhihao@gmail.com>
2020-12-03 11:55:41 -0700

    Add parser function for fromAllToOne

--- api/parser.js
+++ api/parser.js
@@ -1,14 +1,62 @@
 module.exports = class parser {
-    constructor() { this.lists = new Map();}
+    constructor() { 
+        this.simpleMap = new Map();
+        this.jsonObj = {
+            nodes: [],
+            links: []
+        };
+    }
 
     parse(res) {
+        // other is the sum of everything besides top 9
         let other = res.summaryData.totals;
-    
+        // forEach through the data.rows to store nodes into JSON
         res.rows.forEach(element => {
-            other = other - element['data']
-            this.lists.set(element.value, element['data'])
+            // remove the top 9 from total value
+            other = other - element['data'];
+            // set to the map for future use
+            this.simpleMap.set(element.value, element.itemId);
+            // store to JSON
+            this.jsonObj.nodes.push({
+                name: element.value,
+                group: parseInt(element.itemId),
+                index: parseInt(element.itemId),
+                volume: element.data[0]
+            })
         });
-        this.lists.set("Other", other)
-        return this.lists;
+        // store node OTHER into JSON
+        this.jsonObj.nodes.push({
+            name: "Other",
+            group: -1,
+            index: -1,
+            volume: other
+        })
+
+        return this.simpleMap;
+    }
+
+    parseToJson(fromPage, data) {
+        // toOther is the sum of everything besides top 9
+        let toOther = 0;
+        // forEach through the data.rows to store links into JSON
+        data.rows.forEach(element => {
+            if (this.simpleMap.has(element.value)) {
+                // store to JSON
+                this.jsonObj.links.push({
+                    source: parseInt(element.itemId),
+                    target: parseInt(this.simpleMap.get(fromPage)),
+                    value: parseInt(element.data[0])
+                });
+            } else {
+                // toOther of everything besides top 9
+                toOther += parseInt(element['data']);
+            }
+        });
+        // store link to OTHER into JSON
+        this.jsonObj.links.push({
+            source: -1,
+            target: parseInt(this.simpleMap.get(fromPage)),
+            value: toOther
+        });
     }
 }
